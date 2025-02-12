ded8958a178cb7ea6320641578dbc0ae2b4158c6
coco875 <59367621+coco875@users.noreply.github.com>
2025-02-12 11:45:39 +0100

    more complexity

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,144 @@
 #include <spdlog/spdlog.h>
 #include <fstream>
 
+enum {
+    SHADER_0,
+    SHADER_INPUT_1,
+    SHADER_INPUT_2,
+    SHADER_INPUT_3,
+    SHADER_INPUT_4,
+    SHADER_INPUT_5,
+    SHADER_INPUT_6,
+    SHADER_INPUT_7,
+    SHADER_TEXEL0,
+    SHADER_TEXEL0A,
+    SHADER_TEXEL1,
+    SHADER_TEXEL1A,
+    SHADER_1,
+    SHADER_COMBINED,
+    SHADER_NOISE
+};
+
 prism::ContextTypes add_text(std::vector<prism::ContextTypes> args) {
     std::string items = "";
     for (int i = 0; i<args.size(); i++) {
         items += "add";
     }
     return prism::ContextTypes{items};
+}
+
+#define RAND_NOISE "((random(vec3(floor(gl_FragCoord.xy * noise_scale), float(frame_count))) + 1.0) / 2.0)"
+
+static const char* shader_item_to_str(uint32_t item, bool with_alpha, bool only_alpha, bool inputs_have_alpha,
+                                      bool first_cycle, bool hint_single_element) {
+    if (!only_alpha) {
+        switch (item) {
+            case SHADER_0:
+                return with_alpha ? "vec4(0.0, 0.0, 0.0, 0.0)" : "vec3(0.0, 0.0, 0.0)";
+            case SHADER_1:
+                return with_alpha ? "vec4(1.0, 1.0, 1.0, 1.0)" : "vec3(1.0, 1.0, 1.0)";
+            case SHADER_INPUT_1:
+                return with_alpha || !inputs_have_alpha ? "vInput1" : "vInput1.rgb";
+            case SHADER_INPUT_2:
+                return with_alpha || !inputs_have_alpha ? "vInput2" : "vInput2.rgb";
+            case SHADER_INPUT_3:
+                return with_alpha || !inputs_have_alpha ? "vInput3" : "vInput3.rgb";
+            case SHADER_INPUT_4:
+                return with_alpha || !inputs_have_alpha ? "vInput4" : "vInput4.rgb";
+            case SHADER_TEXEL0:
+                return first_cycle ? (with_alpha ? "texVal0" : "texVal0.rgb")
+                                   : (with_alpha ? "texVal1" : "texVal1.rgb");
+            case SHADER_TEXEL0A:
+                return first_cycle
+                           ? (hint_single_element ? "texVal0.a"
+                                                  : (with_alpha ? "vec4(texVal0.a, texVal0.a, texVal0.a, texVal0.a)"
+                                                                : "vec3(texVal0.a, texVal0.a, texVal0.a)"))
+                           : (hint_single_element ? "texVal1.a"
+                                                  : (with_alpha ? "vec4(texVal1.a, texVal1.a, texVal1.a, texVal1.a)"
+                                                                : "vec3(texVal1.a, texVal1.a, texVal1.a)"));
+            case SHADER_TEXEL1A:
+                return first_cycle
+                           ? (hint_single_element ? "texVal1.a"
+                                                  : (with_alpha ? "vec4(texVal1.a, texVal1.a, texVal1.a, texVal1.a)"
+                                                                : "vec3(texVal1.a, texVal1.a, texVal1.a)"))
+                           : (hint_single_element ? "texVal0.a"
+                                                  : (with_alpha ? "vec4(texVal0.a, texVal0.a, texVal0.a, texVal0.a)"
+                                                                : "vec3(texVal0.a, texVal0.a, texVal0.a)"));
+            case SHADER_TEXEL1:
+                return first_cycle ? (with_alpha ? "texVal1" : "texVal1.rgb")
+                                   : (with_alpha ? "texVal0" : "texVal0.rgb");
+            case SHADER_COMBINED:
+                return with_alpha ? "texel" : "texel.rgb";
+            case SHADER_NOISE:
+                return with_alpha ? "vec4(" RAND_NOISE ", " RAND_NOISE ", " RAND_NOISE ", " RAND_NOISE ")"
+                                  : "vec3(" RAND_NOISE ", " RAND_NOISE ", " RAND_NOISE ")";
+        }
+    } else {
+        switch (item) {
+            case SHADER_0:
+                return "0.0";
+            case SHADER_1:
+                return "1.0";
+            case SHADER_INPUT_1:
+                return "vInput1.a";
+            case SHADER_INPUT_2:
+                return "vInput2.a";
+            case SHADER_INPUT_3:
+                return "vInput3.a";
+            case SHADER_INPUT_4:
+                return "vInput4.a";
+            case SHADER_TEXEL0:
+                return first_cycle ? "texVal0.a" : "texVal1.a";
+            case SHADER_TEXEL0A:
+                return first_cycle ? "texVal0.a" : "texVal1.a";
+            case SHADER_TEXEL1A:
+                return first_cycle ? "texVal1.a" : "texVal0.a";
+            case SHADER_TEXEL1:
+                return first_cycle ? "texVal1.a" : "texVal0.a";
+            case SHADER_COMBINED:
+                return "texel.a";
+            case SHADER_NOISE:
+                return RAND_NOISE;
+        }
+    }
+    return "";
+}
+
+prism::ContextTypes append_formula(std::vector<prism::ContextTypes> args) {
+    // uint8_t c[2][4] = 
+    auto c = std::get<prism::MTDArray<int>>(args[0].value); 
+    bool do_single = std::get<bool>(args[1].value);
+    bool do_multiply = std::get<bool>(args[2].value);
+    bool do_mix = std::get<bool>(args[3].value);
+    bool with_alpha = std::get<bool>(args[4].value);
+    bool only_alpha = std::get<bool>(args[5].value);
+    bool opt_alpha = std::get<bool>(args[6].value);
+    bool first_cycle = std::get<bool>(args[7].value);
+    std::string out = "";
+    if (do_single) {
+        out += shader_item_to_str(c.at(only_alpha, 3), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+    } else if (do_multiply) {
+        out += shader_item_to_str(c.at(only_alpha, 0), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += " * ";
+        out += shader_item_to_str(c.at(only_alpha, 2), with_alpha, only_alpha, opt_alpha, first_cycle, true);
+    } else if (do_mix) {
+        out += "mix(";
+        out += shader_item_to_str(c.at(only_alpha, 1), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += ", ";
+        out += shader_item_to_str(c.at(only_alpha, 0), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += ", ";
+        out += shader_item_to_str(c.at(only_alpha, 2), with_alpha, only_alpha, opt_alpha, first_cycle, true);
+        out += ")";
+    } else {
+        out += "(";
+        out += shader_item_to_str(c.at(only_alpha, 0), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += " - ";
+        out += shader_item_to_str(c.at(only_alpha, 1), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += ") * ";
+        out += shader_item_to_str(c.at(only_alpha, 2), with_alpha, only_alpha, opt_alpha, first_cycle, true);
+        out += " + ";
+        out += shader_item_to_str(c.at(only_alpha, 3), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -31,7 +163,11 @@ int main(int argc, char** argv) {
     float o_float[] = { 1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f };
     int o_masks[2] = { 1, 1 };
     int o_blend[2] = { 1, 1 };
-    int o_c[2][2][3] = { { { 1, 1, 1 }, { 1, 1, 1 } }, { { 1, 1, 1 }, { 1, 1, 1 } } };
+    int o_c[2][2][4] = { { { 1, 1, 1, 1 }, { 1, 1, 1, 1 } }, { { 1, 1, 1, 1 }, { 1, 1, 1, 1 } } };
+    int o_color_alpha_same[2] = { 1, 1 };
+    int o_do_single[2][2] = { { 1, 1 }, { 1, 1 } };
+    int o_do_multiply[2][2] = { { 1, 1 }, { 1, 1 } };
+    int o_do_mix[2][2] = { { 1, 1 }, { 1, 1 } };
 
     prism::ContextItems vars = {
         VAR( "o_textures", M_ARRAY(o_textures, int, 2) ),
@@ -46,8 +182,27 @@ int main(int argc, char** argv) {
         VAR("o_three_point_filter", true),
         VAR("o_2cyc", true),
         VAR("o_c", M_ARRAY(o_c, int, 3, 2, 2)),
-        VAR("SHADER_COMBINED", 1),
         VAR("add_text", add_text),
+        VAR("o_color_alpha_same", M_ARRAY(o_color_alpha_same, int, 2)),
+        VAR("SHADER_0", SHADER_0),
+        VAR("SHADER_INPUT_1", SHADER_INPUT_1),
+        VAR("SHADER_INPUT_2", SHADER_INPUT_2),
+        VAR("SHADER_INPUT_3", SHADER_INPUT_3),
+        VAR("SHADER_INPUT_4", SHADER_INPUT_4),
+        VAR("SHADER_INPUT_5", SHADER_INPUT_5),
+        VAR("SHADER_INPUT_6", SHADER_INPUT_6),
+        VAR("SHADER_INPUT_7", SHADER_INPUT_7),
+        VAR("SHADER_TEXEL0", SHADER_TEXEL0),
+        VAR("SHADER_TEXEL0A", SHADER_TEXEL0A),
+        VAR("SHADER_TEXEL1", SHADER_TEXEL1),
+        VAR("SHADER_TEXEL1A", SHADER_TEXEL1A),
+        VAR("SHADER_1", SHADER_1),
+        VAR("SHADER_COMBINED", SHADER_COMBINED),
+        VAR("SHADER_NOISE", SHADER_NOISE),
+        VAR("append_formula", append_formula),
+        VAR("o_do_single", M_ARRAY(o_do_single, int, 2, 2)),
+        VAR("o_do_multiply", M_ARRAY(o_do_multiply, int, 2, 2)),
+        VAR("o_do_mix", M_ARRAY(o_do_mix, int, 2, 2)),
     };
 
     prism::Processor processor;
